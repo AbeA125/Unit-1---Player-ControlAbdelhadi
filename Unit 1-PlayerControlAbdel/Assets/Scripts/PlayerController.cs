69b0efd009fdf8caa9e07e4780e1794084a5f70e
Ibrahim Abdelhadi <727349@student.dusd.net>
2024-03-04 11:40:13 -0800

    Car game
    made player control the movement of the car

--- Unit 1-PlayerControlAbdel/Assets/Scripts/PlayerController.cs
+++ Unit 1-PlayerControlAbdel/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    // Private Variables
+    private float speed = 20;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         //Move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
