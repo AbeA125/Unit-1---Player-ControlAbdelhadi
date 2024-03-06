6bca78a7ad63e05daa7eff724dc93f924076beae
Ibrahim Abdelhadi <727349@student.dusd.net>
2024-03-06 11:27:54 -0800

    Unit 1 player control
    Finished Game

--- Unit 1-PlayerControlAbdel/Assets/Scripts/PlayerController.cs
+++ Unit 1-PlayerControlAbdel/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     // Private Variables
     private float speed = 20;
-    private float turnSpeed = 25.0f;
+    private float turnSpeed = 35.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchkey;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +25,13 @@ public class PlayerController : MonoBehaviour
         // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-        //Move the vehicle foward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //We turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchkey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
